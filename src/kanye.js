fb7c2981da1b3925b7ef84b01e1fa2dd60f7807b
Cameron Hunter <chunter@twitter.com>
2016-04-11 21:39:35 -0700

    Moar kanye

--- src/kanye.js
+++ src/kanye.js
@@ -4,6 +4,8 @@ import { ssml } from 'alexa-ssml';
 import Twitter from './twitter';
 import TwitterConfig from '../config/twitter.config.js';
 
+const random = (items) => items[Math.floor(Math.random() * items.length)];
+
 export class Kanye {
 
   constructor(attributes, client = new Twitter(TwitterConfig)) {
@@ -34,9 +36,49 @@ export class Kanye {
     });
   }
 
+  @Intent('Love')
+  love() {
+    return say('I love Kanye as much as Kanye loves Kanye');
+  }
+
+  @Intent('LoveMe')
+  loveMe() {
+    return this._tweet('715370821368283136');
+  }
+
+  @Intent('Movie')
+  movie() {
+    return this._tweet('703600011884498945');
+  }
+
+  @Intent('Style')
+  style() {
+    return this._tweet('707705291056541697');
+  }
+
+  @Intent('Crazy')
+  crazy() {
+    return this._tweet(random(['702564264008159233', '699503963595472897']));
+  }
+
+  @Intent('Search')
+  search({ query }) {
+    return this.client.getSearch({ q: `${query} from:kanyewest` }).then(([tweet]) => this._tweet(tweet.id_str));
+  }
+
+  @Intent('Wisdom')
+  wisdom() {
+    return say('Wisdom');
+    //return this._collection('id').then((tweets) => this._tweet(random(tweets).id_str));
+  }
+
   @Intent('AMAZON.CancelIntent', 'AMAZON.NoIntent', 'AMAZON.StopIntent')
   goodbye() {
     return say('Goodbye');
+  }
+
+  _tweet(id) {
+    return this.client.getTweet({ id }).then(({ text }) => say(text));
   }
 
   _getTweet(offset = 0) {
--- src/twitter.js
+++ src/twitter.js
@@ -1,13 +1,25 @@
-import { Twitter } from 'twitter-js-client';
+import { Twitter } from 'twitter-node-client';
 
 export default class Client {
   constructor(config) {
     this.client = new Twitter(config);
   }
 
-  getUserTimeline(...args) {
+  getUserTimeline(args) {
+    return this._fetch('getUserTimeline', args);
+  }
+
+  getTweet(args) {
+    return this._fetch('getTweet', args);
+  }
+
+  getSearch(args) {
+    return this._fetch('getSearch', args);
+  }
+
+  _fetch(fn, args) {
     return new Promise((resolve, reject) => {
-      this.client.getUserTimeline(...args, reject, _ => resolve(JSON.parse(_)));
+      this.client[fn](args, reject, response => resolve(JSON.parse(response)));
     });
   }
 }
